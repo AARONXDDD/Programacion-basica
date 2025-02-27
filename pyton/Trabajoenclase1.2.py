c78b95fe070a05d2b77420efb5005f1d7b03fec1
AARONXDDD <almanzaaron78@gmail.com>
2025-02-27 07:46:52 -0600

    .

---
+++ pyton/Trabajoenclase1.2.py
@@ -0,0 +1,9 @@
+creditos = int (input ("ingrese sus creditos: "))
+if creditos >= 80:
+    print("puedes hacer el servicio social.")
+else:
+    print("No tienes los creditos suficientes.")
+if creditos >= 120:
+    print("puedes hacer las residencias.")
+else:
+    print("No puedes hacer las residencias.")
