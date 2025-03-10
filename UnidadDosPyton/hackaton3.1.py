4ca86e0b93470aa162ffc79007d7220e2de005df
AARONXDDD <almanzaaron78@gmail.com>
2025-03-10 07:54:26 -0600

    TrabajoEnClase3.1

---
+++ UnidadDosPyton/hackaton3.1.py
@@ -0,0 +1,9 @@
+lista_del_mandado = []
+
+
+    print ("deseas realizar el mandado?")
+if lista_del_mandado = [si]
+    print ("cuantos producos deseas comprar?")
+
+    producto1 = input("Agrega el primer producto: ")
+lista_del_mandado.append(producto1)
