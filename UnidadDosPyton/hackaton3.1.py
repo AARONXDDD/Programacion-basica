b9900a31c4c4837087cb7ba65d8d757af40ace91
AARONXDDD <almanzaaron78@gmail.com>
2025-03-12 19:43:08 -0600

    Hackaton3.1

--- UnidadDosPyton/hackaton3.1.py
+++ UnidadDosPyton/hackaton3.1.py
@@ -1,9 +1,39 @@
 lista_del_mandado = []
 
+while True:
+    print("\n deseas crear lista del mandado?")
+    print("1. si")
+    print("2. no")
 
-    print ("deseas realizar el mandado?")
-if lista_del_mandado = [si]
-    print ("cuantos producos deseas comprar?")
+    opcion = input("Elige una opción: ")
+    
+    if opcion == "1":
+        producto1 = input("agrega productos a tu carrito de compras: ")
+        lista_del_mandado.append(producto1)
+        print(f"✅ producto '{producto1}' agregado.")
 
-    producto1 = input("Agrega el primer producto: ")
-lista_del_mandado.append(producto1)
+        producto2 = input("agrega productos a tu carrito de compras: ")
+        lista_del_mandado.append(producto2)
+        print(f"✅ producto '{producto2}' agregado.")
+
+        producto3 = input("agrega productos a tu carrito de compras: ")
+        lista_del_mandado.append(producto3)
+        print(f"✅ producto '{producto3}' agregado.")
+
+        producto4 = input("agrega productos a tu carrito de compras: ")
+        lista_del_mandado.append(producto4)
+        print(f"✅ producto '{producto4}' agregado.")
+
+    elif opcion == "2":
+        print("arriba el pri")
+        break
+
+    else:
+        print("Opción inválida. Intenta de nuevo.")
+
+    
+    print("\n Tu lista del mando  es:")
+    for producto in lista_del_mandado:
+        print(f"- {producto}")
+
+   
